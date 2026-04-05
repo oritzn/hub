ea93f963e0ab99aacd28906121933aee2b79fb11
MoritzNoemmerSWH <moritz.noemmer@swh-heider.de>
2026-04-05 19:04:23 +0200

    updated frontend

---
+++ client/script.js
@@ -0,0 +1,9 @@
+const dropZone = document.getElementsById("drop-zone");
+
+dropZone.addEventListener("drop", dropHandler);
+
+window.addEventListener("drop", (e) => {
+  if ([...e.dataTransfer.items].some((item) => item.kind === "file")) {
+    e.preventDefault();
+  }
+});
