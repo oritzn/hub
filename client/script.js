de8e4a67e694ac7c0b1361c977b3cb4ba9de395e
oritzn <oritzn>
2026-04-06 11:22:47 +0200

    Test

--- client/script.js
+++ client/script.js
@@ -1,9 +1,3 @@
 const dropZone = document.getElementsById("drop-zone");
 
 dropZone.addEventListener("drop", dropHandler);
-
-window.addEventListener("drop", (e) => {
-  if ([...e.dataTransfer.items].some((item) => item.kind === "file")) {
-    e.preventDefault();
-  }
-});
